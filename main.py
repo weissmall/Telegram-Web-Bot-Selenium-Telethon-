5154573d9d3da2e16580355f4df3c3fb3163d9db
danysmall <dany.small.work@gmail.com>
2022-03-12 20:38:57 +0300

    • Refactor code • Added function getting images by message id

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from functions import *
 
 # Главная функция
 def main():
-    
+
     # @SCROLL_COUNT — Количество сообщений, при котором можно останавливать скролл
     # @SCROLL_SETTINGS — Настройки скроллинга. Либо мы скроллим в % соотношении от высоты поля
     #                   Либо мы скроллим на определенное количество пикселей
@@ -17,27 +17,27 @@ def main():
     # @TYPICAL_FILE_FORMAT — формат конечного файла(ов)
     # @PATH_TO_FILE — путь до папки, в которую будут сохранятся файлы
     # @HTML_DIVIDER — разделитель, который будет записываться после HTML-кода сообщения
-    
-    SCROLL_COUNT = 1000
+
+    SCROLL_COUNT = 1
     SCROLL_SETTINGS = {'type' : 'percent', 'value' : 50}
     # SCROLL_SETTINGS = {'type' : 'pixel', 'value' : 2000}
-    
+
     TYPICAL_FILENAME = 'message'
     TYPICAL_FILE_FORMAT = 'txt'
     PATH_TO_FILE = 'folder/'
-    
+
     # Создаем переменную с эмулированным Chrome
     driver = webdriver.Chrome('chromedriver.exe')  # Указываем путь до драйвера
-    
-    # Заходим по адресу get(addr) 
+
+    # Заходим по адресу get(addr)
     driver.get('http://web.telegram.org/z/')
-    
+
     # Запускаем функцию поиска чата
     check_for_url(driver)
-    
+
     time.sleep(1)
     wait_for_new_messagesses(driver)
-    elems = scroll_to_top(driver, SCROLL_COUNT, SCROLL_SETTINGS)
+    elems = scroll_to_top(driver, SCROLL_COUNT, SCROLL_SETTINGS, '', 'https://t.me/traficmaker_freelance')
     save_messages(PATH_TO_FILE, TYPICAL_FILENAME, TYPICAL_FILE_FORMAT, elems)
 
 # Если файл запущен как главный, то запускаем главную функцию
@@ -46,4 +46,3 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         print('Exit program by pressing ^C')
-        
--- tg.py
+++ tg.py
@@ -3,7 +3,14 @@ import asyncio
 from telethon.tl.types import InputMessagesFilterPhotos
 
 
-async def get_images(session_name, api_id, api_hash, chat_id, limit):
+async def get_images(
+    session_name: str,
+    api_id: int,
+    api_hash: str,
+    chat_id: str,
+    limit: int,
+    absolute_path: str
+):
     result = dict()
 
     client = TelegramClient(session_name, api_id, api_hash)
@@ -15,23 +22,100 @@ async def get_images(session_name, api_id, api_hash, chat_id, limit):
             limit=limit
         ):
             if message.media is not False:
-                temp = await client.download_media(message.media, 'images/')
+                temp = await client.download_media(
+                    message.media,
+                    apply_path(absolute_path, 'images/')
+                )
                 result[message.id] = temp
 
         return result
 
 
-def run_get_images():
-    result = asyncio.run(get_images(
+def apply_path(abs_path, img_path):
+    return '{}{}'.format(abs_path, img_path)
+
+
+async def __get_media_by_id(
+    session_name,
+    api_id,
+    api_hash,
+    chat_id,
+    message_id
+):
+    client = TelegramClient(session_name, api_id, api_hash)
+    async with client:
+        await client.start()
+        await client.get_messages(chat_id, ids=message_id)
+
+        result = list()
+        message = await client.get_messages(chat_id, ids=message_id)
+        if message.grouped_id is None:
+            if message.media is not False:
+                temp = await client.download_media(
+                    message.media,
+                    'images/'
+                )
+                result.append(temp)
+        else:
+            grouped_id = message.grouped_id
+            while message.grouped_id == grouped_id:
+                if message.media is not False:
+                    temp = await client.download_media(
+                        message.media,
+                        'images/'
+                    )
+                    result.append(temp)
+                message_id += 1
+                message = await client.get_messages(chat_id, ids=message_id)
+
+        return result
+
+
+def get_media_by_id(session_name, api_id, api_hash, chat_id, message_id):
+    return asyncio.run(__get_media_by_id(
+        api_hash=api_hash,
+        api_id=api_id,
+        session_name=session_name,
+        chat_id=chat_id,
+        message_id=message_id
+    ))
+
+
+def run_get_images(
+    api_hash,
+    api_id,
+    session_name,
+    chat_id,
+    limit,
+    absolute_path
+):
+    return asyncio.run(get_images(
+        api_hash=api_hash,
+        api_id=api_id,
+        session_name=session_name,
+        chat_id=chat_id,
+        limit=limit,
+        absolute_path=absolute_path
+    ))
+
+
+if __name__ == '__main__':
+    # run_get_images(
+    #     api_hash='3778859a51ffe2951f3abe886d03d0f1',
+    #     api_id=7212719,
+    #     session_name='session',
+    #     chat_id='https://t.me/moodnight_af',
+    #     limit=5
+    # )
+
+    res = get_media_by_id(
         api_hash='3778859a51ffe2951f3abe886d03d0f1',
         api_id=7212719,
         session_name='session',
         chat_id='https://t.me/moodnight_af',
-        limit=5
-    ))
+        message_id=114
+    )
+    print(res)
 
-    return result
-
-
-if __name__ == '__main__':
-    run_get_images()
+    # grouped_id=13176146443563802
+    # grouped_id=13176146443563802

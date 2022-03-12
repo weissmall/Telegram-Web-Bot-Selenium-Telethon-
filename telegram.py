fe47e238e0ffe8961c1c3aca827794259ec01bdc
danysmall <dany.small.work@gmail.com>
2022-03-12 15:30:21 +0300

    Added needed functions with Getting images list by Telethon and TelegramAPI

--- telegram.py
+++ telegram.py
@@ -1,4 +1,6 @@
 import errors
+from telethon import TelegramClient
+import asyncio
 
 
 class TelegramBot():
@@ -48,8 +50,40 @@ class TelegramBot():
                 type(session_name)
             )
 
-    def get_messages_list(self) -> list:
-        pass
+        self.session_name = session_name
+        self.api_id = api_id
+        self.api_hash = api_hash
+
+    async def start_session(self):
+        self.client = TelegramClient(
+            self.session_name,
+            self.api_id,
+            self.api_hash
+        )
+
+        await self.client.start()
+
+    def __get_messages_list(self, group_id: int | str):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(
+            self.__get_messages_list(group_id)
+        )
+
+    async def get_messages_list(self, group_id: int | str) -> list:
+        if not isinstance(group_id, int) and not isinstance(group_id, str):
+            raise errors.TelegramTypeError(
+                'get_messages_list()',
+                'Incorrect type of "group_id"',
+                str,
+                type(group_id)
+            )
+
+        result = list()
+        async with self.client:
+            async for message in self.client.get_messages(group_id):
+                result.append(message)
+
+        return result
 
     def get_images_src_dict(self, messages: list) -> dict:
         if not isinstance(messages, list):
@@ -75,10 +109,20 @@ class TelegramBot():
         return output_dict
 
 
-# if __name__ == '__main__':
-#     bot = TelegramBot(
-#         api_id=100,
-#         api_hash='asdfges432',
-#         session_name='session'
-#     )
-#     bot.get_images_src_dict([])
+async def main():
+    bot = TelegramBot(
+        api_id=7212719,
+        api_hash='3778859a51ffe2951f3abe886d03d0f1',
+        session_name='session'
+    )
+    await bot.start_session()
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(bot.get_messages_list('https://t.me/gray_kardinal_chat'))
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(main())
+    # main()
